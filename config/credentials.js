2d29e9d55730fe0147342570ac25765d50e805e4
Manoj <manojk030303@gmail.com>
2025-03-05 18:51:28 +0530

    updated config

--- config/credentials.js
+++ config/credentials.js
@@ -1,5 +1,5 @@
 const credentials = {
-    domain: 'http://localhost:3000'
+    domain: process.env.AUTH_URL || process.env.NEXT_BASE_URL || 'http://localhost:3000'
 };
 
 module.exports = credentials;
